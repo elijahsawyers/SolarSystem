d4f9842204fc490e8735da0be3c11e3469bb552b
Elijah Sawyers <elijahsawyers@gmail.com>
2020-10-29 14:32:03 -0500

    Transitions to make things smoother

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@
  */
 
 let STAR_COUNT  = 250;
-let PLANET_SIZE = 15;
+let ORBITING_PLANET_SIZE = 15;
 let NUM_PLANETS = 8;
 
 /**
@@ -23,10 +23,11 @@ window.onload = () => {
   let sizeText = document.getElementById("size-text");
   let distanceText = document.getElementById("distance-text");
   let midPoint = { x: container.clientWidth / 2 + container.offsetLeft, y: container.clientHeight / 2 };
-  let planetPadding = (((container.clientHeight / 2) - (PLANET_SIZE * NUM_PLANETS)) / 7);
+  let planetPadding = (((container.clientHeight / 2) - (ORBITING_PLANET_SIZE * NUM_PLANETS)) / 7);
   let sun, mercury, venus, earth, mars, jupiter, saturn, uranus, neptune;
   let speedScale = 1.0;
   let speedInterval;
+  let animating = false;
 
   /*==================================================
   Event handlers
@@ -40,6 +41,7 @@ window.onload = () => {
 
   for (let i = 0; i < menuButtons.length; i++) {
     menuButtons[i].onclick = (e) => {
+      if (animating) { return; }
       menuButtons[currentButtonSelected].classList.remove("selected");
       menuButtons[i].classList.add("selected");
 
@@ -77,106 +79,127 @@ window.onload = () => {
 
   let initSpeed = () => {
     if (currentButtonSelected == 0) { return; }
-    let orbitPaths = document.getElementsByClassName("orbit-path");
-    for (let i = 0; i < orbitPaths.length; i++) { orbitPaths[i].classList.remove("hidden"); }
-    let arrows = document.getElementsByClassName("arrow");
-    while(arrows.length) { arrows[0].remove(); }
     speedSlider.classList.remove("no-display");
     sizeText.classList.add("no-display");
     distanceText.classList.add("no-display");
     speedText.classList.remove("no-display");
-    sun.setSize(15, 15);
-    mercury.setSize(15, 15);
-    venus.setSize(15, 15);
-    earth.setSize(15, 15);
-    mars.setSize(15, 15);
-    jupiter.setSize(15, 15);
-    saturn.setSize(15, 15);
-    uranus.setSize(15, 15);
-    neptune.setSize(15, 15);
-    sun.setPosition(midPoint.x, midPoint.y);
-    mercury.setPosition(midPoint.x, midPoint.y - (planetPadding * 1));
-    venus.setPosition(midPoint.x, midPoint.y - (planetPadding * 2));
-    earth.setPosition(midPoint.x, midPoint.y - (planetPadding * 3));
-    mars.setPosition(midPoint.x, midPoint.y - (planetPadding * 4));
-    jupiter.setPosition(midPoint.x, midPoint.y - (planetPadding * 5));
-    saturn.setPosition(midPoint.x, midPoint.y - (planetPadding * 6));
-    uranus.setPosition(midPoint.x, midPoint.y - (planetPadding * 7));
-    neptune.setPosition(midPoint.x, midPoint.y - (planetPadding * 8));
-    speedInterval = setInterval(() => { // Start the orbital loop - 1 day is 10 miliseconds, so an earth year is 3.65 seconds.
-      mercury.rotate(midPoint, -4.091 * speedScale);
-      venus.rotate(midPoint, -1.600 * speedScale);
-      earth.rotate(midPoint, -0.986 * speedScale);
-      mars.rotate(midPoint, -0.524 * speedScale);
-      jupiter.rotate(midPoint, -0.082 * speedScale);
-      saturn.rotate(midPoint, -0.034 * speedScale);
-      uranus.rotate(midPoint, -0.012 * speedScale);
-      neptune.rotate(midPoint, -0.006 * speedScale);
-    }, 10);
-    currentButtonSelected = 0;
+
+    animating = true;
+    container.style.animation = "fade-out-in 1s ease";
+    setTimeout(() => {
+      let orbitPaths = document.getElementsByClassName("orbit-path");
+      for (let i = 0; i < orbitPaths.length; i++) { orbitPaths[i].classList.remove("hidden"); }
+      let arrows = document.getElementsByClassName("arrow");
+      while(arrows.length) { arrows[0].remove(); }
+      sun.setSize(15, 15);
+      mercury.setSize(15, 15);
+      venus.setSize(15, 15);
+      earth.setSize(15, 15);
+      mars.setSize(15, 15);
+      jupiter.setSize(15, 15);
+      saturn.setSize(15, 15);
+      uranus.setSize(15, 15);
+      neptune.setSize(15, 15);
+      sun.setPosition(midPoint.x, midPoint.y);
+      mercury.setPosition(midPoint.x, midPoint.y - (planetPadding * 1));
+      venus.setPosition(midPoint.x, midPoint.y - (planetPadding * 2));
+      earth.setPosition(midPoint.x, midPoint.y - (planetPadding * 3));
+      mars.setPosition(midPoint.x, midPoint.y - (planetPadding * 4));
+      jupiter.setPosition(midPoint.x, midPoint.y - (planetPadding * 5));
+      saturn.setPosition(midPoint.x, midPoint.y - (planetPadding * 6));
+      uranus.setPosition(midPoint.x, midPoint.y - (planetPadding * 7));
+      neptune.setPosition(midPoint.x, midPoint.y - (planetPadding * 8));
+      speedInterval = setInterval(() => { // Start the orbital loop - 1 day is 10 miliseconds, so an earth year is 3.65 seconds.
+        mercury.rotate(midPoint, -4.091 * speedScale);
+        venus.rotate(midPoint, -1.600 * speedScale);
+        earth.rotate(midPoint, -0.986 * speedScale);
+        mars.rotate(midPoint, -0.524 * speedScale);
+        jupiter.rotate(midPoint, -0.082 * speedScale);
+        saturn.rotate(midPoint, -0.034 * speedScale);
+        uranus.rotate(midPoint, -0.012 * speedScale);
+        neptune.rotate(midPoint, -0.006 * speedScale);
+      }, 10);
+      currentButtonSelected = 0;
+    }, 500);
+
+    setTimeout(() => {container.style.animation = null; animating = false;}, 1000);
   };
 
   let initSize = () => {
-    let orbitPaths = document.getElementsByClassName("orbit-path");
-    for (let i = 0; i < orbitPaths.length; i++) { orbitPaths[i].classList.add("hidden"); }
-    let arrows = document.getElementsByClassName("arrow");
-    while(arrows.length) { arrows[0].remove(); }
     speedSlider.classList.add("no-display");
-    window.clearInterval(speedInterval);
     speedText.classList.add("no-display");
     distanceText.classList.add("no-display");
     sizeText.classList.remove("no-display");
-    sun.setSize(864, 864); sun.setPosition(window.innerWidth - 150, midPoint.y - (864 / 2));
-    mercury.setSize(3.03, 3.03); mercury.setPosition(window.innerWidth - (50 * 4), midPoint.y - (3.03 / 2));
-    venus.setSize(7.52, 7.52); venus.setPosition(window.innerWidth - (50 * 5), midPoint.y - (7.52 / 2));
-    earth.setSize(7.92, 7.92); earth.setPosition(window.innerWidth - (50 * 6), midPoint.y - (7.92 / 2));
-    mars.setSize(4.21, 4.21); mars.setPosition(window.innerWidth - (50 * 7), midPoint.y - (4.21 / 2));
-    jupiter.setSize(86.89, 86.89); jupiter.setPosition(window.innerWidth - (50 * 8) - 86.89, midPoint.y - (86.89 / 2));
-    saturn.setSize(72.37, 72.37); saturn.setPosition(window.innerWidth - (50 * 9) - (86.89 + 72.37), midPoint.y - (72.37 / 2));
-    uranus.setSize(31.52, 31.52); uranus.setPosition(window.innerWidth - (50 * 10) - (86.89 + 72.37 + 31.52), midPoint.y - (31.52 / 2));
-    neptune.setSize(30.60, 30.60); neptune.setPosition(window.innerWidth - (50 * 11) - (86.89 + 72.37 + 31.52 + 30.60), midPoint.y - (30.60 / 2));
-    container.append(createArrow(window.innerWidth - (50 * 4), midPoint.y - (3.03 / 2) - 45));
-    container.append(createArrow(window.innerWidth - (50 * 5), midPoint.y - (7.52 / 2) - 45));
-    container.append(createArrow(window.innerWidth - (50 * 6), midPoint.y - (7.92 / 2) - 45));
-    container.append(createArrow(window.innerWidth - (50 * 7), midPoint.y - (4.21 / 2) - 45));
-    currentButtonSelected = 1;
+
+    animating = true;
+    container.style.animation = "fade-out-in 1s ease";
+    setTimeout(() => {
+      let orbitPaths = document.getElementsByClassName("orbit-path");
+      for (let i = 0; i < orbitPaths.length; i++) { orbitPaths[i].classList.add("hidden"); }
+      let arrows = document.getElementsByClassName("arrow");
+      while(arrows.length) { arrows[0].remove(); }
+      window.clearInterval(speedInterval);
+      sun.setSize(864, 864); sun.setPosition(window.innerWidth - 150, midPoint.y - (864 / 2));
+      mercury.setSize(3.03, 3.03); mercury.setPosition(window.innerWidth - (50 * 4), midPoint.y - (3.03 / 2));
+      venus.setSize(7.52, 7.52); venus.setPosition(window.innerWidth - (50 * 5), midPoint.y - (7.52 / 2));
+      earth.setSize(7.92, 7.92); earth.setPosition(window.innerWidth - (50 * 6), midPoint.y - (7.92 / 2));
+      mars.setSize(4.21, 4.21); mars.setPosition(window.innerWidth - (50 * 7), midPoint.y - (4.21 / 2));
+      jupiter.setSize(86.89, 86.89); jupiter.setPosition(window.innerWidth - (50 * 8) - 86.89, midPoint.y - (86.89 / 2));
+      saturn.setSize(72.37, 72.37); saturn.setPosition(window.innerWidth - (50 * 9) - (86.89 + 72.37), midPoint.y - (72.37 / 2));
+      uranus.setSize(31.52, 31.52); uranus.setPosition(window.innerWidth - (50 * 10) - (86.89 + 72.37 + 31.52), midPoint.y - (31.52 / 2));
+      neptune.setSize(30.60, 30.60); neptune.setPosition(window.innerWidth - (50 * 11) - (86.89 + 72.37 + 31.52 + 30.60), midPoint.y - (30.60 / 2));
+      container.append(createArrow(window.innerWidth - (50 * 4), midPoint.y - (3.03 / 2) - 45));
+      container.append(createArrow(window.innerWidth - (50 * 5), midPoint.y - (7.52 / 2) - 45));
+      container.append(createArrow(window.innerWidth - (50 * 6), midPoint.y - (7.92 / 2) - 45));
+      container.append(createArrow(window.innerWidth - (50 * 7), midPoint.y - (4.21 / 2) - 45));
+      currentButtonSelected = 1;
+    }, 500);
+
+    setTimeout(() => {container.style.animation = null; animating = false;}, 1000);
   };
 
   let initDistance = () => {
-    let orbitPaths = document.getElementsByClassName("orbit-path");
-    for (let i = 0; i < orbitPaths.length; i++) { orbitPaths[i].classList.add("hidden"); }
-    let arrows = document.getElementsByClassName("arrow");
-    while(arrows.length) { arrows[0].remove(); }
     speedSlider.classList.add("no-display");
     sizeText.classList.add("no-display");
     speedText.classList.add("no-display");
     distanceText.classList.remove("no-display");
-    window.clearInterval(speedInterval);
-    sun.setSize(5, 5);
-    mercury.setSize(5, 5);
-    venus.setSize(5, 5);
-    earth.setSize(5, 5);
-    mars.setSize(5, 5);
-    jupiter.setSize(5, 5);
-    saturn.setSize(5, 5);
-    uranus.setSize(5, 5);
-    neptune.setSize(5, 5);
-    let au = 27.5;
-    let startingX = 100 + window.innerWidth - container.clientWidth;
-    sun.setPosition(startingX, midPoint.y);
-    mercury.setPosition(startingX + (au * 0.31), midPoint.y);
-    venus.setPosition(startingX + (au * 0.72), midPoint.y);
-    earth.setPosition(startingX + au, midPoint.y);
-    mars.setPosition(startingX + (au * 1.44), midPoint.y);
-    jupiter.setPosition(startingX + (au * 5.12), midPoint.y);
-    saturn.setPosition(startingX + (au * 9.98), midPoint.y);
-    uranus.setPosition(startingX + (au * 19.7), midPoint.y);
-    neptune.setPosition(startingX + (au * 29.92), midPoint.y);
-    container.append(createArrow(startingX + (au * 5.12), midPoint.y - 42.5));
-    container.append(createArrow(startingX + (au * 9.98), midPoint.y - 42.5));
-    container.append(createArrow(startingX + (au * 19.7), midPoint.y - 42.5));
-    container.append(createArrow(startingX + (au * 29.92), midPoint.y - 42.5));
-    currentButtonSelected = 2;
+
+    animating = true;
+    container.style.animation = "fade-out-in 1s ease";
+    setTimeout(() => {
+      let orbitPaths = document.getElementsByClassName("orbit-path");
+      for (let i = 0; i < orbitPaths.length; i++) { orbitPaths[i].classList.add("hidden"); }
+      let arrows = document.getElementsByClassName("arrow");
+      while(arrows.length) { arrows[0].remove(); }
+      window.clearInterval(speedInterval);
+      sun.setSize(5, 5);
+      mercury.setSize(5, 5);
+      venus.setSize(5, 5);
+      earth.setSize(5, 5);
+      mars.setSize(5, 5);
+      jupiter.setSize(5, 5);
+      saturn.setSize(5, 5);
+      uranus.setSize(5, 5);
+      neptune.setSize(5, 5);
+      let au = 27.5;
+      let startingX = 100 + window.innerWidth - container.clientWidth;
+      sun.setPosition(startingX, midPoint.y);
+      mercury.setPosition(startingX + (au * 0.31), midPoint.y);
+      venus.setPosition(startingX + (au * 0.72), midPoint.y);
+      earth.setPosition(startingX + au, midPoint.y);
+      mars.setPosition(startingX + (au * 1.44), midPoint.y);
+      jupiter.setPosition(startingX + (au * 5.12), midPoint.y);
+      saturn.setPosition(startingX + (au * 9.98), midPoint.y);
+      uranus.setPosition(startingX + (au * 19.7), midPoint.y);
+      neptune.setPosition(startingX + (au * 29.92), midPoint.y);
+      container.append(createArrow(startingX + (au * 5.12), midPoint.y - 42.5));
+      container.append(createArrow(startingX + (au * 9.98), midPoint.y - 42.5));
+      container.append(createArrow(startingX + (au * 19.7), midPoint.y - 42.5));
+      container.append(createArrow(startingX + (au * 29.92), midPoint.y - 42.5));
+      currentButtonSelected = 2;
+    }, 500);
+
+    setTimeout(() => {container.style.animation = null; animating = false;}, 1000);
   };
 
   /*==================================================
@@ -250,8 +273,8 @@ class Planet extends AstronomicalObject {
     this.orbitPath.classList.add("orbit-path");
     this.orbitPath.style.width = width + "px";
     this.orbitPath.style.height = height + "px";
-    this.orbitPath.style.left = this.x - (width / 2) + (PLANET_SIZE / 2) + "px";
-    this.orbitPath.style.top = this.y + (PLANET_SIZE / 2) + "px";
+    this.orbitPath.style.left = this.x - (width / 2) + (ORBITING_PLANET_SIZE / 2) + "px";
+    this.orbitPath.style.top = this.y + (ORBITING_PLANET_SIZE / 2) + "px";
     this.orbitPath.style.borderRadius = width / 2 + "px";
     this.container.append(this.orbitPath);
   }
